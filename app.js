d6bfa32a012b1c0aca962f1ffaae68d116653ede
Eleaha <ellieharrington8@gmail.com>
2024-04-15 15:25:24 +0100

    adds get /api/articles by ID endpoint, 404 and 400 error handling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { getTopics} = require('./controllers/topics.controllers');
+const { getTopics } = require('./controllers/topics.controllers');
 const { getEndpoints } = require('./controllers/api.controllers');
-
+const { getArticleById } = require('./controllers/articles.controllers');
 
 const app = express();
 
@@ -9,9 +9,24 @@ app.get('/api', getEndpoints);
 
 app.get('/api/topics', getTopics);
 
+app.get('/api/articles/:article_id', getArticleById);
+
 app.all('*', (req, res, next) => {
 	res.status(404).send({ msg: '404 - not found' });
-	next();
+});
+
+app.use((err, req, res, next) => {
+	if (err.status && err.msg) {
+		res.status(err.status).send({ msg: err.msg });
+	}
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	if (err.code === '22P02') {
+		res.status(400).send({ msg: 'Bad request' });
+	}
+	next(err)
 });
 
 app.use((err, req, res, next) => {
