d509f3f65d018ac1f79ae8d872c9c532881fa221
Eleaha <ellieharrington8@gmail.com>
2024-04-17 14:57:11 +0100

    adds topic query for /api/articles

--- models/articles.models.js
+++ models/articles.models.js
@@ -11,24 +11,30 @@ function fetchArticleById(id) {
 		});
 }
 
-function fetchArticles() {
-	return db
-		.query(
-			`SELECT 
+function fetchArticles(topic) {
+	let dbQuery = `SELECT 
             articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.article_img_url, 
             CAST(COALESCE(SUM(comments.votes), 0) AS INTEGER) 
             AS votes, 
             CAST(COUNT(comments.article_id) AS INTEGER) 
             AS comment_count 
-            FROM articles 
+            FROM articles
             LEFT JOIN comments 
-            ON comments.article_id = articles.article_id 
-            GROUP BY articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.article_img_url
-            ORDER BY created_at DESC;`
-		)
-		.then(({ rows }) => {
-			return rows;
-		});
+            ON comments.article_id = articles.article_id`;
+
+	let queryValues = [];
+
+	if (topic) {
+		dbQuery += ` WHERE topic=$1`;
+		queryValues.push(topic);
+	}
+
+	dbQuery += ` GROUP BY articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.article_img_url 
+	ORDER BY created_at DESC;`;
+
+	return db.query(dbQuery, queryValues).then(({ rows }) => {
+		return rows;
+	});
 }
 
 function checkArticleExists(id) {
@@ -38,24 +44,33 @@ function checkArticleExists(id) {
 			if (!rows.length) {
 				return Promise.reject({
 					status: 404,
-					msg: 'Article not found',
+					msg: 'Not found',
 				});
 			}
 		});
 }
 
-function updateVotesById(id, votes){
-	return db.query(`
+function updateVotesById(id, votes) {
+	return db
+		.query(
+			`
 	UPDATE articles
 	SET votes = votes + $1
 	WHERE article_id=$2
-	RETURNING *`, [votes, id])
-	.then(({rows}) => {
-		if (!rows.length) {
-			return Promise.reject({ status: 404, msg: 'Article not found' });
-		}
-		return rows[0]
-	})
+	RETURNING *`,
+			[votes, id]
+		)
+		.then(({ rows }) => {
+			if (!rows.length) {
+				return Promise.reject({ status: 404, msg: 'Article not found' });
+			}
+			return rows[0];
+		});
 }
 
-module.exports = { fetchArticleById, fetchArticles, checkArticleExists, updateVotesById };
+module.exports = {
+	fetchArticleById,
+	fetchArticles,
+	checkArticleExists,
+	updateVotesById,
+};
