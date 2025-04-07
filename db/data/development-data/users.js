441d2a4321fd9dddf4795617d7b00bbc7bb7fde6
Ellie <147191486+Eleaha@users.noreply.github.com>
2025-04-07 17:11:39 +0100

    Add more test users

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -34,5 +34,30 @@ module.exports = [
     name: 'Jess Jelly',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'
+  },
+    {
+    "username": "john_doe",
+    "name": "John Doe",
+    "avatar_url": "https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/activities-fun/10-great-small-dog-breeds/maltese-portrait.jpg?h=448&w=740&hash=B111F1998758CA0ED2442A4928D5105D"
+  },
+  {
+    "username": "sarah_smith",
+    "name": "Sarah Smith",
+    "avatar_url": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Orange_tabby_kitten.jpg"
+  },
+  {
+    "username": "mike_123",
+    "name": "Mike Johnson",
+    "avatar_url": ""
+  },
+  {
+    "username": "lily_walker",
+    "name": "Lily Walker",
+    "avatar_url": ""
+  },
+  {
+    "username": "emily_brown",
+    "name": "Emily Brown",
+    "avatar_url": ""
   }
 ];
